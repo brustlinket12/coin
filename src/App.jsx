d34b05c668eaf11ab2c8996a28555624c3309696
brustlinket12 <jor9124@gmail.com>
2024-11-15 19:58:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import PaginaInicio from './Paginas/PaginaInicio'; 
-import PaginaTutorial from './Paginas/PaginaMonto';
-import PaginaInicioSesion from './Paginas/PaginaInicioSesion';
-import PaginaInicioRegistrar from './Paginas/PaginaInicioRegistrar';
-import PaginaDashboard from './Paginas/PaginaDashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PaginaInicio from './Paginas/PaginaInicio'; // Página de inicio
+import PaginaTutorial from './Paginas/PaginaMonto'; // Página de tutorial
+import PaginaInicioSesion from './Paginas/PaginaInicioSesion'; // Página de inicio de sesión
+import PaginaInicioRegistrar from './Paginas/PaginaInicioRegistrar'; // Página de registro
+import PaginaDashboard from './Paginas/PaginaDashboard'; // Página del dashboard
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <PaginaInicioRegistrar/>
-      {/* <PaginaTutorial/>
-      <PaginaInicioSesion/>
-      <PaginaInicioRegistrar/> 
-      <PaginaDashboard/>
-      */}
-    </div>
+    <Router>
+      <Routes>
+        {/* Ruta inicial (por defecto) */}
+        <Route path="/" element={<PaginaInicio />} /> {/* Página de inicio como página por defecto */}
+
+        {/* Otras rutas */}
+        <Route path="/tutorial" element={<PaginaTutorial />} />
+        <Route path="/inicio-sesion" element={<PaginaInicioSesion />} />
+        <Route path="/registrar" element={<PaginaInicioRegistrar />} />
+        <Route path="/dashboard" element={<PaginaDashboard />} />
+        {/* Agrega más rutas si es necesario */}
+      </Routes>
+    </Router>
   );
 }
 
+export default App;
